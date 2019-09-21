3a8745b81b4dfd6d7b30032a081a12971e5cb77e
kamran khan <48378125+kamran1231@users.noreply.github.com>
2019-09-21 18:52:56 +0530

    Add files via upload

---
+++ inheritance1.py
@@ -0,0 +1,31 @@
+# INHERITANCE METHOD
+
+class Mobile:
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    def display_all(self):
+        return f'MOBILE: {self.brand}, MODEL: {self.model} and PRICE: {self.price}'
+
+
+class Smartphone(Mobile):
+
+    # two ways we use the inheritance:
+
+    def __init__(self, brand, model, price, ram, internal_memory, rear_camera):
+        # 1st method:
+        # Mobile.__init__(self,brand,model,price)
+        # 2nd Method:
+        super().__init__(brand, model, price)
+        self.ram = ram
+        self.internal_memory = internal_memory
+        self.rear_camera = rear_camera
+
+
+mob1 = Mobile('nokia', 1108, 3000)
+mob2 = Smartphone('samsung', 'qeqwq', 40000, '16GB', '30GB', '25megaPIxcel')
+print(mob1.display_all())
+print(mob2.price)
+print(mob2.display_all())
